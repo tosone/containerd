20fa23c99d8a136a0ab3c69a67815e1e6797d49e
Tosone <i@tosone.cn>
2023-10-13 17:05:11 +0800

    Add message to error output

--- remotes/errors/errors.go
+++ remotes/errors/errors.go
@@ -17,9 +17,14 @@
 package errors
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/containerd/log"
+	dtspecv1 "github.com/opencontainers/distribution-spec/specs-go/v1"
 )
 
 var _ error = ErrUnexpectedStatus{}
@@ -30,23 +35,38 @@ type ErrUnexpectedStatus struct {
 	StatusCode                int
 	Body                      []byte
 	RequestURL, RequestMethod string
+	Message                   string
 }
 
 func (e ErrUnexpectedStatus) Error() string {
-	return fmt.Sprintf("unexpected status from %s request to %s: %s", e.RequestMethod, e.RequestURL, e.Status)
+	return fmt.Sprintf("unexpected status(%s) from %s request to %s: %s", e.Status, e.RequestMethod, e.RequestURL, e.Message)
 }
 
 // NewUnexpectedStatusErr creates an ErrUnexpectedStatus from HTTP response
 func NewUnexpectedStatusErr(resp *http.Response) error {
 	var b []byte
+	var message string
 	if resp.Body != nil {
 		b, _ = io.ReadAll(io.LimitReader(resp.Body, 64000)) // 64KB
+		if len(b) > 0 {
+			var r dtspecv1.ErrorResponse
+			err := json.Unmarshal(b, &r)
+			if err != nil {
+				log.G(context.Background()).Debugf("Unmarshal response body failed: %v", err)
+			}
+			for _, e := range r.Errors {
+				if len(e.Message) > 0 {
+					message = e.Message
+				}
+			}
+		}
 	}
 	err := ErrUnexpectedStatus{
 		Body:          b,
 		Status:        resp.Status,
 		StatusCode:    resp.StatusCode,
 		RequestMethod: resp.Request.Method,
+		Message:       message,
 	}
 	if resp.Request.URL != nil {
 		err.RequestURL = resp.Request.URL.String()
